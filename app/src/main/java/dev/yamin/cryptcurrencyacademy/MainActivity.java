0f1bf229a04be55ed847b18404d38c3c57fd020c
yamin elmakis <yaminelmakis@gmail.com>
2018-03-04 09:23:51 +0200

    update dependencies

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
@@ -49,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // test 2
+    // https://api.binance.com
+
+    // https://api.binance.com/api/v1/exchangeInfo
+
+    // https://api.binance.com/api/v1/ticker/24hr?symbol=LTCUSDT
+
+    // https://api.binance.com/api/v3/ticker/price?symbol=LTCUSDT
+
+    // https://api.binance.com/api/v1/klines?symbol=LTCUSDT&interval=5m&limit=5
 }
