b490e87f535fe8a191e18a6d8eb28af6f9cbaee3
yamin elmakis <yaminelmakis@gmail.com>
2018-03-05 20:09:48 +0200

    minor change - not done

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // test aksjdakj
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
