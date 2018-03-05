5d6766d8237aa473f716717dd65de06b44c56665
yamin elmakis <yaminelmakis@gmail.com>
2018-03-05 19:51:24 +0200

    minor change

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             return true;
         }
