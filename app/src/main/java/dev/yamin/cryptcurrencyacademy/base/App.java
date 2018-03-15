172383819450939cb2119a9690a4571387aa1157
yamin elmakis <yaminelmakis@gmail.com>
2018-03-15 17:58:26 +0200

    add logs + update utils

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/base/App.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/base/App.java
@@ -1,8 +1,28 @@
 package dev.yamin.cryptcurrencyacademy.base;
 
+import android.app.Application;
+
+import java.util.Locale;
+
+import dev.yamin.cryptcurrencyacademy.BuildConfig;
+import lib.yamin.easylog.EasyLog;
+import lib.yamin.easylog.EasyLogFormatter;
+
 /**
  * Created by Yamin on 3/10/2018.
  */
 
-public class App {
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EasyLog.showLogs(BuildConfig.DEBUG);
+        EasyLog.setFormatter(new EasyLogFormatter() {
+            @Override
+            public String format(String classname, String methodName, int lineNumber) {
+                return String.format(Locale.getDefault(), "%s.%s()[%d] => ", classname, methodName, lineNumber);
+            }
+        });
+    }
 }
--- app/src/main/java/dev/yamin/cryptcurrencyacademy/utils/AppUtils.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/utils/AppUtils.java
@@ -1,8 +1,67 @@
 package dev.yamin.cryptcurrencyacademy.utils;
 
+import android.graphics.Color;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.StringReader;
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Random;
+
 /**
  * Created by Yamin on 3/8/2018.
  */
 
 public class AppUtils {
+
+    private static final Gson gson;
+    private static final Random random;
+    private static SimpleDateFormat formater;
+
+    private AppUtils() {    }
+
+    static {
+        GsonBuilder builder = new GsonBuilder();
+        gson = builder.create();
+        random = new Random(System.currentTimeMillis());
+        formater = new SimpleDateFormat("EEE", Locale.getDefault());
+    }
+
+
+    public static String objToJsonStr(Object object) {
+        return gson.toJson(object);
+    }
+
+    public static <T> T getObjectFromStr(String jsonStr, final Class<T> classOfT) throws com.google.gson.JsonSyntaxException {
+        JsonReader reader = new JsonReader(new StringReader(jsonStr));
+        reader.setLenient(true);
+        return gson.fromJson(reader, classOfT);
+    }
+
+    public static <T> T getObjectFromStr(String jsonStr, Type typeOfT) throws com.google.gson.JsonSyntaxException {
+        JsonReader reader = new JsonReader(new StringReader(jsonStr));
+        reader.setLenient(true);
+        return gson.fromJson(reader, typeOfT);
+    }
+
+    public static int randomColor() {
+        int alpha = random.nextInt(255), red = random.nextInt(255), green = random.nextInt(255), blue = random.nextInt(255);
+        return Color.argb(alpha, red, green, blue);
+    }
+
+    public static int randomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+
+    public static String getFullDateTime(long timeInMillis) {
+        timeInMillis *= 60;
+        timeInMillis *= 1000;
+        formater.applyPattern("dd-MMM");
+        return formater.format(timeInMillis);
+    }
 }
