7d73ea978fdc12cadf5cacb828aa83827519d16a
Lior25 <lior2512@gmail.com>
2018-03-15 18:02:10 +0200

    Tabs NOT DONE

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/my/coins/CoinsFragment.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/my/coins/CoinsFragment.java
@@ -57,16 +57,16 @@ public class CoinsFragment extends BaseFragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+   @Override
+     public void onAttach(Context context) {
+         super.onAttach(context);
+         if (context instanceof OnFragmentInteractionListener) {
+             mListener = (OnFragmentInteractionListener) context;
+         } else {
+             throw new RuntimeException(context.toString()
+                     + " must implement OnFragmentInteractionListener");
+         }
+     }
 
     @Override
     public void onDetach() {
