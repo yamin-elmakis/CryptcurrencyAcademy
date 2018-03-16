d3233b79eb60ff43b93ec24b66018eb5eab7234b
yamin elmakis <yaminelmakis@gmail.com>
2018-03-16 06:19:12 +0200

    add DataUtils

--- app/src/main/java/dev/yamin/cryptcurrencyacademy/utils/DataUtils.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/utils/DataUtils.java
@@ -10,22 +10,28 @@ import java.util.ArrayList;
 
 public class DataUtils {
 
-    public static final String Litecoin = "LTC";
     public static final String Bitcoin = "BTC";
     public static final String Ethereum = "ETH";
     public static final String Ripple = "XRP";
     public static final String Bitcoin_Cash = "BCH";
+    public static final String Litecoin = "LTC";
+    public static final String Neo = "NEO";
     public static final String Monero = "XMR";
     public static final String Dash = "DASH";
-    public static final String Neo = "NEO";
 
     private DataUtils() {}
 
     @NonNull
     public ArrayList<String> getSupportedCoins() {
         ArrayList<String> coins = new ArrayList<>(10);
-        coins.add("");
-
+        coins.add(Bitcoin);
+        coins.add(Ethereum);
+        coins.add(Ripple);
+        coins.add(Bitcoin_Cash);
+        coins.add(Litecoin);
+        coins.add(Neo);
+        coins.add(Monero);
+        coins.add(Dash);
         return coins;
     }
 }
