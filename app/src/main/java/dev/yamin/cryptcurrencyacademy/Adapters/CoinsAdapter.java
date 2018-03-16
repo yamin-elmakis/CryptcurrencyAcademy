4ed43ae2528ba51ff78c39af79ecb56398632390
yuval <yuvalmetal@gmail.com>
2018-03-16 02:42:40 +0200

    added item view

---
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/Adapters/CoinsAdapter.java
@@ -0,0 +1,49 @@
+package dev.yamin.cryptcurrencyacademy.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import dev.yamin.cryptcurrencyacademy.Adapters.RecyclerViewAdapter;
+import dev.yamin.cryptcurrencyacademy.R;
+import dev.yamin.cryptcurrencyacademy.network.POJOS.Coin24Hr;
+
+/**
+ * Created by yuval on 16/03/2018.
+ */
+
+class CoinsAdapter extends RecyclerViewAdapter<Coin24Hr> {
+
+    public CoinsAdapter(Context context, OnViewHolderClick<Coin24Hr> listener) {
+        super(context, listener);
+    }
+
+    @Override
+    protected View createView(Context context, ViewGroup viewGroup, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_coin_view, viewGroup, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        return v;    }
+
+    @Override
+    protected void bindView(Coin24Hr item, RecyclerViewAdapter.ViewHolder viewHolder) {
+            if(item != null){
+                TextView coinName = (TextView) viewHolder.getView(R.id.coin_name);
+                TextView coinPrice = (TextView) viewHolder.getView(R.id.coin_price);
+                TextView coinPercent = (TextView) viewHolder.getView(R.id.percent);
+
+                coinName.setText(item.getSymbol());
+                coinPrice.setText("$" + item.getLastPrice());
+                coinPercent.setText(item.());
+
+            }
+
+    }
+
+
+}
--- app/src/main/java/dev/yamin/cryptcurrencyacademy/my/coins/CoinsFragment.java
+++ app/src/main/java/dev/yamin/cryptcurrencyacademy/my/coins/CoinsFragment.java
@@ -3,6 +3,7 @@ package dev.yamin.cryptcurrencyacademy.my.coins;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ public class CoinsFragment extends BaseFragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private RecyclerView mRecyclerView;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -47,7 +49,10 @@ public class CoinsFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_coins, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_coins, container, false);
+
+        mRecyclerView = rootView.findViewById(R.id.recyclerView);
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
